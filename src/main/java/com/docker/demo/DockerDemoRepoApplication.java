d6ae2568419b67229b98fb560f2d214e95dc32b2
unknown <maheshkumar.iare@gmail.com>
2020-08-29 16:54:16 +0530

    First commit to my git repo

---
+++ src/main/java/com/docker/demo/DockerDemoRepoApplication.java
@@ -0,0 +1,13 @@
+package com.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoRepoApplication.class, args);
+	}
+
+}
